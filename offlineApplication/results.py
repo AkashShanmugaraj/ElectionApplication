76624734bf06ee544e6f5260a047fc6b912766a4
AkashShanmugaraj <akashthelezend@gmail.com>
2022-06-09 22:08:01 +0530

    Added save functions and better output

--- offlineApplication/results.py
+++ offlineApplication/results.py
@@ -1,6 +1,9 @@
 import json
 import time
+dataStorage = []
+
 def result(post):
+    finalString = ''
     f = open('data.json', 'r')
     data = json.load(f)
     post_dict = data[post]
@@ -11,9 +14,12 @@ def result(post):
         votes.append(post_dict[i]['votes'])
     try:
         highest = max(votes)
-    except:
+    except ValueError:
         highest = 0
-        print('The post is empty')
+        finalString = f'For {post}, no candidature found!'
+        writeResult('arraySave', finalString)
+        print(finalString)
+        return None
     votesindex = []
     for i in votes:
         if i == highest:
@@ -21,16 +27,31 @@ def result(post):
             votes[votes.index(i)] = 'Counted'
     print(f'Results generated for {post}')
     if len(votesindex) == 1:
-        print('The winner for the specified post is: ')
-        print("Name: "+ post_dict[f'candidate{votesindex[0] + 1}']['name'])
-        print(f'Votes: {highest}')
+        finalString = f'The winner for the {post} post is '+post_dict[f'candidate{votesindex[0] + 1}']['name']+f' with {highest} votes'
+
     elif len(votesindex) > 1:
-        print('Tie breaker for:')
+        finalString = f'There is a Tiebreaker for the post of {post} between '
         for i in range(len(votesindex)):
-            print("Name: "+ post_dict[f'candidate{votesindex[i] + 1}']['name'])
-        print(f'Everyone above secured {highest} votes each')
+            finalString += post_dict[f'candidate{votesindex[i] + 1}']['name']
+            if i != len(votesindex)-1:
+                finalString += ', '
+            else:
+                finalString += ' '
+        finalString += f'at {highest} votes'
+    print(finalString)
+    writeResult('arraySave', finalString)
     print('\n\n')
 
+def writeResult(mode, data = None):
+    if mode == 'arraySave':
+        dataStorage.append(data)
+    elif mode == 'diskSave':
+        fileHandle = open('Results.txt', 'w')
+        for statement in dataStorage:
+            fileHandle.write(statement)
+            fileHandle.write('\n')
+        fileHandle.close()
+        print('Data sucessfully saved at Results.txt')
 
 # Function call
 result('maleSPL')
@@ -48,4 +69,12 @@ result('femalePALLAVA')
 result('maleCulturalSEC')
 result('femaleCulturalSEC')
 result('SwachBarathCoord')
+
+saveQ = int(input('Do you want to save the following data in your computer?\n1: YES\n2: NO\n>>> '))
+while saveQ not in [1,2]:
+    print('Invalid input, use either 1 or 2.')
+    saveQ = int(input('Do you want to save the following data in your computer?\n1: YES\n2: NO\n>>> '))
+if saveQ == 1:
+    writeResult('diskSave')
+
 time.sleep(5)
